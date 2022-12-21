3878a2ef95b7ad53843475c51a750a86f460edb0
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-12-21 21:51:21 +0530

    Update NEWTON_FORWARD.c

--- NEWTON_FORWARD.c
+++ NEWTON_FORWARD.c
@@ -3,7 +3,7 @@
 void display(int n,float arr[n][n+1])
 {
     for(int i=0;i<n;i++){
-        for(int j=0;j<n+1;j++)
+        for(int j=0;j<n+1-i;j++)
         {
             printf("%0.2f ",arr[i][j]);
         }
